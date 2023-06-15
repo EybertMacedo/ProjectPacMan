5872908e0bc07136603508be113f2afb1667a923
Graciela Condori Anahua <69116570+gcondoria@users.noreply.github.com>
2023-06-14 23:45:32 -0500

    Update item.h  agregado de atributos y sus  getters y setters

--- assets/item.h
+++ assets/item.h
@@ -1,7 +1,18 @@
 #include <iostream>
-
+#include <string>
 class item
-{
+{   
+    private:
+        int posicion;
+        string tipo;
+        bool estado;
     public:
-    virtual item() = 0;
+        virtual item();
+        virtual ~item();
+        virtual int getPosicion() = 0;
+        virtual void setPosicion(int) = 0;
+        virtual string getTipo() = 0;
+        virtual void setTipo(string) = 0;
+        virtual bool getEstado() = 0;
+        virtual void setEstado(bool) = 0;
 };
