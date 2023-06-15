31a98fef7fa148a336fd2d82a40ecdb46cc0013c
LucelyMendoza <91226029+LucelyMendoza@users.noreply.github.com>
2023-06-14 23:42:04 -0500

    Actualizar clase personaje.h

--- assets/personaje.h
+++ assets/personaje.h
@@ -1,1 +1,12 @@
+class Personaje {
+protected:
+    int posX;  // Posición X del personaje en el laberinto
+    int posY;  // Posición Y del personaje en el laberinto
 
+public:
+    Personaje(int x, int y) : posX(x), posY(y) {}
+
+    virtual void mover() = 0;  // Método virtual puro para el movimiento del personaje
+
+    // Otros métodos y atributos comunes a todos los personajes
+};
