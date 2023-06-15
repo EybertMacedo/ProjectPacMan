6af64de599ad12dad96845474113d5547436faa9
Rommel Paredes Banda <91158430+RommelPa@users.noreply.github.com>
2023-06-15 13:32:34 -0500

    Update interfaz.h

--- assets/interfaz.h
+++ assets/interfaz.h
@@ -1,1 +1,31 @@
+class Interfaz {
+public:
+    virtual void metodo() = 0;
+    virtual void otroMetodo() {
+        // Implementación opcional
+    }
+    
+    // Destructor virtual
+    virtual ~Interfaz() {}
+};
 
+// Clase derivada que implementa la interfaz
+class ClaseDerivada : public Interfaz {
+public:
+    void metodo() override {
+        // Implementación del método
+    }
+};
+
+int main() {
+
+    Interfaz* miPuntero = new ClaseDerivada();
+    
+    // Llamar al método de la interfaz a través del puntero
+    miPuntero->metodo();
+    
+    // Liberar memoria
+    delete miPuntero;
+    
+    return 0;
+}
